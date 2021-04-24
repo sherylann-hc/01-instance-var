7c9c06c92acb6fed9fe559a6898f1c7e3b49fd0f
tiobagio <tio@hashicorp.com>
2021-04-24 22:55:57 +0800

    another commit

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "region" {
   description = "The region where the resources are created."
-  default     = "us-east-1"
+  default     = "us-east-2"
 }
 
-variable "az" {
+variable "zone" {
   description = "The region where the resources are created."
-  default     = "us-east-1a"
+  default     = "us-east-2a"
 }
 
 variable "instance_type" {
