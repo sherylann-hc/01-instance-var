56b0e5fe94e7471f777f67bd71d054650b1ef70c
sherylann-hc <84066766+sherylann-hc@users.noreply.github.com>
2021-08-19 10:24:13 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,10 +6,12 @@ resource "aws_instance" "app" {
   instance_type     = "${var.instance_type}"
   availability_zone = "${var.zone}"
   ami               = "ami-0c55b159cbfafe1f0"
-}
-
-
- tags = {
-Name = "app-${count.index}"
+  
+  tags = {
+    Name = "app-${count.index}"
+  }
  }
 }
+
+
+ 
